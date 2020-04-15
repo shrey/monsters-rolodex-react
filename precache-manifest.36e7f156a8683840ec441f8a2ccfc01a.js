9366b1fb54d383d28aa74111fce0e649a76087c3
Shrey Dubey <wshrey09@gmail.com>
2020-04-15 18:39:38 +0530

    Updates

---
+++ precache-manifest.36e7f156a8683840ec441f8a2ccfc01a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af246b4981c8fbab4aa9d8d1b63607cc",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "c33e89e418bb94a5b2b2",
+    "url": "/monsters-rolodex-react/static/css/main.f0b0a7e7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex-react/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33e89e418bb94a5b2b2",
+    "url": "/monsters-rolodex-react/static/js/main.b16be4f5.chunk.js"
+  },
+  {
+    "revision": "826edb68318ff1203c46",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.82ff8142.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
